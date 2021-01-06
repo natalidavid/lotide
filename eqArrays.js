efed54db1bfc227cd93194fdf910259b5a4ad0ff
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-06 06:38:16 +0000

    Function that can compare two arrays for a perfect match

--- eqArrays.js
+++ eqArrays.js
@@ -8,11 +8,11 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arrOne, arrTwo) {
-  for (var i = 0; i < arrOne.length; i++) {
-		if (arrOne[i] !== arrTwo[i]) {
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
       return false;
     }
-	}
+  }
   return true;
 };
 
@@ -21,7 +21,7 @@ const eqArrays = function(arrOne, arrTwo) {
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2"g, "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false */
 
 
