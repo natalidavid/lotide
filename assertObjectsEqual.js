b4ec628f686304ebc472d055bf1f02b82a244f72
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-09 23:13:27 +0000

    Comparing 2 objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const eqObjects = function(object1, object2) {
+  let key1 = Object.keys(object1);
+  let key2 = Object.keys(object2);
+  if (key1.length !== key2.length) {
+    return false;
+  }
+  for (let item of key1) {
+    if (Array.isArray(object1[item])) {
+      if (!Array.isArray(object2[item])) {
+        return false;
+      } else if (!eqArrays(object1[item], object2[item])) {
+        return false;
+      }
+    } else if (object1[item] !== object2[item]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Array Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Array Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
