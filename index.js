0d00c70e7d4fc677b39465ff7b0699cebd190ee4
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-12 03:09:16 +0000

    Index of all Lotide functions

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const head   = require('./head');
+const middle = require('./middle');
+const tail   = require('./tail');
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  head,
+  middle,
+  tail,
+};
