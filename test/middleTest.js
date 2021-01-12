821ea5afa8d775b5dd0e3536de9e92bc320a67f5
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-12 02:34:39 +0000

    Added M&C to tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
+describe("#middle", () => {
+  it("returns empty array for arrays with 1 or 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), [])
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [4]); // => [3]
+  it("returns single element for odd no of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [5]);
+  it("returns array of 2 elements for odd even no of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,8 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(words.length, 8);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs'])
+  });
+});
